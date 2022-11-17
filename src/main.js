d5162d30ce08b6936fe7cd972486c1a38b4820bb
kenreibman <reibmanken@gmail.com>
2022-11-17 18:58:27 -0500

    update

--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
-import { createApp } from 'vue'
-import { Quasar } from 'quasar'
-import { createRouter, createWebHistory } from "vue-router";
+import { createApp } from 'vue';
+import { Quasar } from 'quasar';
+import { createRouter, createWebHistory } from 'vue-router';
 
+import '@quasar/extras/material-icons/material-icons.css';
+import '@quasar/extras/mdi-v6/mdi-v6.css';
+import 'quasar/src/css/index.sass';
+import { Notify, Loading, Dialog } from 'quasar';
 
-import '@quasar/extras/material-icons/material-icons.css'
-import '@quasar/extras/mdi-v6/mdi-v6.css'
-import 'quasar/src/css/index.sass'
-import { Notify, Loading } from 'quasar'
-
-import routes from "./routes";
-import App from './App.vue'
-const myApp = createApp(App)
+import routes from './routes';
+import App from './App.vue';
+const myApp = createApp(App);
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
 createApp(App)
-  .use(Quasar, { plugins: { Notify, Loading, Dialog}, })
+  .use(Quasar, { plugins: { Notify, Loading, Dialog } })
   .use(router)
-  .mount('#app')
+  .mount('#app');
--- src/pages/AdminSite.vue
+++ src/pages/AdminSite.vue
@@ -1,6 +1,16 @@
 <script setup>
 import { ref } from 'vue'
 import services from '@/services'
+import { useQuasar, useDialogPluginComponent } from 'quasar'
+
+const $q = useQuasar()
+const { dialogRef, onDialogHide, onDialogCancel } = useDialogPluginComponent()
+
+function onDialogOK () {
+  services.createEvent(eventName.value).then((res) => {
+    console.log(res)
+  })
+}
 
 services.getEvents().then((res) => {
     events.value = res
@@ -16,6 +26,19 @@ const onSubmit = () => {
   console.log('onSubmit')
 }
 
+function modal () {
+  $q.dialog({
+    title: 'Create new Event',
+    message: 'Some message'
+  }).onOk(() => {
+    // console.log('OK')
+  }).onCancel(() => {
+    // console.log('Cancel')
+  }).onDismiss(() => {
+    // console.log('I am triggered on both OK and Cancel')
+  })
+}
+
 function onRejected (rejectedEntries) {
   // Notify plugin needs to be installed
   // https://quasar.dev/quasar-plugins/notify#Installation
@@ -26,13 +49,33 @@ function onRejected (rejectedEntries) {
 }
 
 const events = ref([])
+const openModal = ref(false)
+const eventName = ref('')
 
 </script>
 
 <template>
   <q-page padding>
   <section class="s-events__forum">
-    <q-btn class="s-events__form-button" color="primary" label="New Event" />
+    <q-btn class="s-events__form-button" color="primary" label="New Event" @click="openModal = true"/>
+
+    <q-dialog ref="dialogRef" @hide="onDialogHide" v-model="openModal">
+      <q-card class="q-dialog-plugin">
+        // event_name (required)
+        <q-input
+        filled
+        v-model="eventName"
+        label="Event Name *"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+      // prize (optional)
+        <q-card-actions align="right">
+          <q-btn color="primary" label="Create" @click="onDialogOK" />
+          <q-btn color="primary" label="Cancel" @click="onDialogCancel" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </section>
     <section class="s-events">
       <h2 class="section-title">Events (pending)</h2>
@@ -46,7 +89,7 @@ const events = ref([])
           </q-card-section>
 
           <q-card-actions>
-            <q-btn class="c-events__view" flat color="primary" to="admin/gallery">View</q-btn>
+            <q-btn class="c-events__view" flat color="primary" to="`admin/${event.id}`">View</q-btn>
           </q-card-actions>
         </q-card>
       </div>
